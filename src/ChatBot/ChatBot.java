e8cfbe2b03727ff4f73a8c81e0fd54b542b1a7fd
Kulachenko Ivan <i.i.kulachenko@student.khai.edu>
2025-10-08 15:42:34 +0300

    ChatBot 2nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,8 +1,16 @@
 package ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
         System.out.println("Hello! My name is Ivan.");
         System.out.println("I was created in 2025.");
+        System.out.println("Please, remind me your name.");
+
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+
+        System.out.println("What a great name you have, " + name + "!");
     }
 }
