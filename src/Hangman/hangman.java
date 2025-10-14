fad062c95bdc731c313ffa5330a05c6093ee44ca
Kulachenko Ivan <i.i.kulachenko@student.khai.edu>
2025-10-14 17:13:08 +0300

    Hangman 8th stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -9,58 +9,68 @@ public class hangman {
         Random random = new Random();
 
         System.out.println("HANGMAN");
-        System.out.println();
 
-        String[] words = {"python", "java", "javascript", "kotlin"};
-        String correctWord = words[random.nextInt(words.length)];
+        while (true) {
+            System.out.print("Type \"play\" to play the game, \"exit\" to quit: > ");
+            String menu = scanner.nextLine();
 
-        StringBuilder hint = new StringBuilder("-".repeat(correctWord.length()));
-        String guessedLetters = "";
-
-        int attempts = 8;
-
-        while (attempts > 0) {
-            System.out.println(hint);
-            System.out.print("Input a letter: > ");
-            String letter = scanner.nextLine();
-
-            if (letter.length() != 1) {
-                System.out.println("You should input a single letter");
-                continue;
-            }
-
-            if (!letter.matches("[a-z]")) {
-                System.out.println("Please enter a lowercase English letter");
-                continue;
-            }
-
-            if (guessedLetters.contains(letter)) {
-                System.out.println("You've already guessed this letter");
-                continue;
-            }
-
-            guessedLetters += letter;
-
-            if (correctWord.contains(letter)) {
-                for (int i = 0; i < correctWord.length(); i++) {
-                    if (correctWord.charAt(i) == letter.charAt(0)) {
-                        hint.setCharAt(i, letter.charAt(0));
-                    }
-                }
-            } else {
-                System.out.println("That letter doesn't appear in the word");
-                attempts--;
-            }
-
-            if (hint.toString().equals(correctWord)) {
-                System.out.println("You guessed the word " + correctWord + "!");
-                System.out.println("You survived!");
+            if (menu.equals("exit")) {
                 break;
+            } else if (!menu.equals("play")) {
+                continue;
             }
-        }
 
-        if (!hint.toString().equals(correctWord) && attempts == 0) {
-            System.out.println("You lost!");
+            String[] words = {"python", "java", "javascript", "kotlin"};
+            String correctWord = words[random.nextInt(words.length)];
+
+            StringBuilder hint = new StringBuilder("-".repeat(correctWord.length()));
+            String guessedLetters = "";
+
+            int attempts = 8;
+
+            while (attempts > 0) {
+                System.out.println(hint);
+                System.out.print("Input a letter: > ");
+                String letter = scanner.nextLine();
+
+                if (letter.length() != 1) {
+                    System.out.println("You should input a single letter");
+                    continue;
+                }
+
+                if (!letter.matches("[a-z]")) {
+                    System.out.println("Please enter a lowercase English letter");
+                    continue;
+                }
+
+                if (guessedLetters.contains(letter)) {
+                    System.out.println("You've already guessed this letter");
+                    continue;
+                }
+
+                guessedLetters += letter;
+
+                if (correctWord.contains(letter)) {
+                    for (int i = 0; i < correctWord.length(); i++) {
+                        if (correctWord.charAt(i) == letter.charAt(0)) {
+                            hint.setCharAt(i, letter.charAt(0));
+                        }
+                    }
+                } else {
+                    System.out.println("That letter doesn't appear in the word");
+                    attempts--;
+                }
+
+                if (hint.toString().equals(correctWord)) {
+                    System.out.println("You guessed the word " + correctWord + "!");
+                    System.out.println("You survived!");
+                    break;
+                }
+            }
+
+            if (!hint.toString().equals(correctWord) && attempts == 0) {
+                System.out.println("You lost!");
+            }
         }
 
         scanner.close();
