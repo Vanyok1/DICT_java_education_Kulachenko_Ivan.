5725f4264a625a13e9c12e8abec76f1d4c7fda8a
Kulachenko Ivan <i.i.kulachenko@student.khai.edu>
2025-10-22 20:33:55 +0300

    CoffeeMachine 3rd stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -11,7 +11,7 @@ public class CoffeeMachine {
         System.out.println("Pouring coffee into the cup");
         System.out.println("Pouring some milk into the cup");
         System.out.println("Coffee is ready!");
-        System.out.println("");
+        System.out.println();
 
         Scanner input = new Scanner(System.in);
 
@@ -26,5 +26,32 @@ public class CoffeeMachine {
         System.out.println(water + " ml of water");
         System.out.println(milk + " ml of milk");
         System.out.println(coffeeBeans + " g of coffee beans");
+
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int Water = input.nextInt();
+
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int Milk = input.nextInt();
+
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int CoffeeBeans = input.nextInt();
+
+        System.out.println("Write how many cups of coffee you will need:");
+        int Cups = input.nextInt();
+
+        int maxCupsByWater = Water / 200;
+        int maxCupsByMilk = Milk / 50;
+        int maxCupsByBeans = CoffeeBeans / 15;
+
+        int maxCupsPossible = Math.min(maxCupsByWater, Math.min(maxCupsByMilk, maxCupsByBeans));
+
+        if (maxCupsPossible == Cups) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (maxCupsPossible > Cups) {
+            int extra = maxCupsPossible - Cups;
+            System.out.println("Yes, I can make that amount of coffee (and even " + extra + " more than that)");
+        } else {
+            System.out.println("No, I can make only " + maxCupsPossible + " cups of coffee");
+        }
     }
 }
