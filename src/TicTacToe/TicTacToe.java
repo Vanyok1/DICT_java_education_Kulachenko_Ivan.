f7954688817f8a7e59cd7f9f0f7a702314faa006
Kulachenko Ivan <i.i.kulachenko@student.khai.edu>
2025-10-28 16:19:12 +0200

    TicTacToe 2nd stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,8 +1,13 @@
 package TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
-        GameBoard board = new GameBoard();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter cells: ");
+        String input = scanner.nextLine();
+        GameBoard board = new GameBoard(input);
         board.displayBoard();
     }
 }
@@ -10,20 +15,25 @@ public class TicTacToe {
 class GameBoard {
     private char[][] board;
 
-    public GameBoard() {
-        board = new char[][]{
-                {'X', 'O', 'X'},
-                {'O', 'X', 'O'},
-                {'X', 'X', 'O'}
-        };
+    public GameBoard(String input) {
+        board = new char[3][3];
+        int index = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = input.charAt(index++);
+            }
+        }
     }
 
     public void displayBoard() {
+        System.out.println("---------");
         for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
             for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j] + " ");
             }
-            System.out.println();
+            System.out.println("|");
         }
+        System.out.println("---------");
     }
 }
