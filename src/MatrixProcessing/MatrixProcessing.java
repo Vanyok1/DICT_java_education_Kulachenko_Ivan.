29161b3e42c4d84963c9b7d11a2282fbd164fe0e
Kulachenko Ivan <i.i.kulachenko@student.khai.edu>
2025-11-07 15:58:13 +0200

    MatrixProcessing 2nd stage

--- src/MatrixProcessing/MatrixProcessing.java
+++ src/MatrixProcessing/MatrixProcessing.java
@@ -7,32 +7,20 @@ public class MatrixProcessing {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n1 = sc.nextInt();
-        int m1 = sc.nextInt();
-        double[][] dataA = new double[n1][m1];
-        for (int i = 0; i < n1; i++) {
-            for (int j = 0; j < m1; j++) {
-                dataA[i][j] = sc.nextDouble();
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        double[][] data = new double[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                data[i][j] = sc.nextDouble();
             }
         }
-        Matrix A = new Matrix(dataA);
+        Matrix matrix = new Matrix(data);
 
-        int n2 = sc.nextInt();
-        int m2 = sc.nextInt();
-        double[][] dataB = new double[n2][m2];
-        for (int i = 0; i < n2; i++) {
-            for (int j = 0; j < m2; j++) {
-                dataB[i][j] = sc.nextDouble();
-            }
-        }
-        Matrix B = new Matrix(dataB);
+        double constant = sc.nextDouble();
 
-        Matrix sum = A.add(B);
-        if (sum == null) {
-            System.out.println("ERROR");
-        } else {
-            sum.print();
-        }
+        Matrix result = matrix.multiplyByConstant(constant);
+        result.print();
     }
 }
 
@@ -50,14 +38,11 @@ class Matrix {
         }
     }
 
-    public Matrix add(Matrix other) {
-        if (this.rows != other.rows || this.cols != other.cols) {
-            return null;
-        }
+    public Matrix multiplyByConstant(double c) {
         double[][] result = new double[rows][cols];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                result[i][j] = this.data[i][j] + other.data[i][j];
+                result[i][j] = this.data[i][j] * c;
             }
         }
         return new Matrix(result);
